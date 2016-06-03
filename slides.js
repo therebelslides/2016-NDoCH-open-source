63c9038c40868790ba9e044896970dc3b4ded4f6
Trent Oswald <trentoswald@therebelrobot.com>
2016-06-03 08:58:21 -0700

    Update slides.js

--- slides.js
+++ slides.js
@@ -1,7 +1,7 @@
 window.__slides = [
   {
     loc: [0, 5],
-    note: 'https://therebelrobot.com/open-source-workshop'
+    note: 'https://slides.therebelrobot.com'
   }, {
     loc: [6, 7]
   }, {
@@ -48,38 +48,6 @@ window.__slides = [
   }, {
     loc: [75,83]
   }, {
-    loc: [58, 69],
-    note: 'https://github.com/therebelrobot/open-source-workshop#start-your-own'
-  }, {
     loc: [85,86]
-  }, {
-    loc: [86, 87],
-    note: 'much more daunting'
-  }, {
-    loc: [88, 94],
-    note: 'Interpersonal skillz ftw'
-  }, {
-    loc: [95,101]
-  }, {
-    loc: [101,102],
-    note: 'Most open source contributions'
-  }, {
-    loc: [103,107],
-    note: 'STOP! Before anything...'
-  }, {
-    loc: [108, 114],
-    note: 'No coding required!'
-  }, {
-    loc: [114,118]
-  }, {
-    loc: [119, 131],
-    note: 'No coding required!'
-  }, {
-    loc: [132, 133],
-    note: 'Issues all over Github that need help'
-  },{
-    loc: [0, 5],
-    title: 'Let\'s dive in!',
-    note: 'https://therebelrobot.com/open-source-workshop'
   }
 ]
