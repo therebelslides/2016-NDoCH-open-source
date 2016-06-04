be21f2d1c3a4f4c9e1471183e4e8748e5041c5db
Trent Oswald <trentoswald@therebelrobot.com>
2016-06-04 10:01:38 -0700

    feat(slides): updated slides with new content

--- code.js
+++ code.js
@@ -1,94 +1,55 @@
-window.__code = 'function Contributing_to (openSource) {\n' +
+window.__code = 'function Best_Practices_for (openSource) {\n' +
   '  // Trent Oswald \n' +
   '  //    Infrastructure Core Team Lead - Code for San Francisco \n' +
-  '  //    @therebelrobot\n' +
+  '  //    therebelrobot.com - @therebelrobot\n' +
   '  // National Day of Civic Hacking, June 4th 2016\n' +
   '\n' +
-  '  what_is(openSource);\n' +
+  '  openSource !== freeLunch()\n' +
+  '  openSource === trust()\n' +
   '\n' +
-  '  (openSource !== freeSoftware);\n' +
+  '  trust.access_source()\n' +
+  '  trust.legally()\n' +
+  '  trust.contribute()\n' +
+  '  trust.governance_and_management()\n' +
+  '  trust.upgrading_and_mantaining()\n' +
+  '  trust.safe_space_to_work()\n' +
   '\n' +
-  '  openSource = "source code available to the general public with relaxed or permissive copyright restrictions";\n' +
-  '\n' +
-  '  openSource = [\n' +
-  '    "Rich and Thriving Community",\n' +
-  '    "Shared Values",\n' +
-  '    "Open Governance",\n' +
-  '    "Ability and Freedom to Iterate",\n' +
-  '    "Passion for Innovation"\n' +
-  '  ]\n' +
-  '\n' +
-  '  openSource << FreeSoftwareFoundation(1997)\n' +
-  '\n' +
-  '  openSourceInitiative(1998)\n' +
-  '\n' +
-  '  Netscape() // Now Mozilla Firefox\n' +
-  '  Debian()\n' +
-  '  LinuxKernel()\n' +
-  '\n' +
-  '  openSource = [\n' +
-  '    "Media Content", // Wikimedia Foundation\n' +
-  '    "Hardware", // Raspberry Pi, Arduino\n' +
-  '    "Beverages",// Open Source Cola, Brewtopia\n' +
-  '    "Firearms", // 3D Printed firearms\n' +
-  '    "Medicine", // Tropical Disease Initiative\n' +
-  '    "Religion" // Open Siddur Project, Open Source Yoga Unity\n' +
-  '  ]\n' +
-  '\n' +
-  '  var whatMakesOpenSource = function () {\n' +
+  '  var howDoIEstablishThisTrust = function () {\n' +
   '    releaseOfSourceCode() \n' +
-  '            // Github, Gitlab, Bitbucket, etc\n' +
-  '    publicAndPermissiveLicense() \n' +
-  '            // MIT, ISC, GPLv3, Apache, etc\n' +
-  '    clearDocumentation() \n' +
-  '            // README, CONTRIBUTING, CHANGELOG, CODEOFCONDUCT, etc.\n' +
-  '  }\n' +
-  '\n' +
-  '  React // Facebook\n' +
-  '  RubyOnRails // David Hansson\n' +
-  '  Linux // The Linux Foundation\n' +
-  '  Android // Google\n' +
-  '  .NETframework // C# - Microsoft\n' +
-  '\n' +
-  '  var howDoIOpenSource = function () {\n' +
-  '\n' +
-  '    url("http://ossmanifesto.org")\n' +
-  '    values = [\n' +
-  '      "Respect",\n' +
-  '      "Collaboration",\n' +
-  '      "Healthy Debates",\n' +
-  '      "Basic Documentation",\n' +
-  '      "Consistent Versioning of releases"\n' +
-  '    ]\n' +
-  '\n' +
-  '    releaseOfSourceCode()\n' +
-  '      // Github\n' +
-  '      // Gitlab\n' +
-  '      // Bitbucket\n' +
-  '    publicAndPermissiveLicense()\n' +
-  '      // MIT (Most Common)\n' +
-  '      // ISC (MIT+)\n' +
-  '      // Apache 2\n' +
-  '      // GNU GPLv3\n' +
-  '    clearDocumentation()\n' +
-  '      // Installation Instructions (README)\n' +
-  '      // Changelog (README or CHANGELOG)\n' +
-  '        // How to file an issue\n' +
-  '        // What to include\n' +
-  '        // How to contribute code\n' +
-  '      // Contributing Guide (README or CONTRIBUTING)\n' +
-  '      // Styling Guides (README or CONTRIBUTING)\n' +
-  '      // Code of Conduct (README or CODEOFCONDUCT)\n' +
-  '\n' +
-  '    testing() // Tap, Mocha, Nightwatch\n' +
-  '    publishing() // NPM, Gems, LuaRocks\n' +
-  '    evangelizing() \n' +
-  '      // Github, HackerNews, Reddit, Brigade Network, etc.\n' +
-  '\n' +
-  '    stayActive() \n' +
-  '      // Github Issues\n' +
-  '      // Slack channel\n' +
-  '      // Email\n' +
-  '  \n' +
+  '            // Github, Gitlab, Bitbucket, etc\n' + 
+  '    publicAndPermissiveLicense() \n' + // choosealicense.org tldrlegal.com
+  '            // LICENSE\n' +
+  '            // MIT, ISC\n' +
+  '            // Apache 2\n' +
+  '            // GPLv3\n' +
+  '            // WTFPL\n' +
+  '    contributingGuide() \n' + // branch management, pull request policies, style guides
+  '            // CONTRIBUTING.md\n' +
+  '            // How to get started\n' +
+  '            // How to add/request new features\n' +
+  '            // Issue triaging\n' +
+  '            // Language Translating\n' +
+  '            // Interacting with the Community\n' +
+  '            // Branch Management Flow\n' +
+  '            // Pull Request Policies\n' +
+  '            // Coding Style Guides\n' +
+  '    workInTheOpen() \n' +
+  '            // Meeting Minutes\n' +
+  '            // Github Issues\n' +
+  '            // Slack\n' +
+  '            // Google Hangouts on Air\n' +
+  '    changelogAndVersioning() \n' +
+  '            // CHANGELOG.md\n' + // http://keepachangelog.org
+  '            // Semantic Versioning\n' + // http://semver.com
+  '    testingAndCoverage() \n' +
+  '            // unit testing\n' +
+  '            // integration testing\n' + 
+  '            // testing coverage\n' + // http://codeclimate.com
+  '    codeOfConduct() \n' +
+  '            // CODEOFCONDUCT.md\n' +
+  '            // Contributor Covenant\n' + // http://contributor-covenant.org/
+  '    upToDateDocumentation() \n' +
+  '       // Its not open unless its documented.\n' +
+  '       // Its not useful unless its current.\n' +
   '  }\n' +
   '}'
--- slides.js
+++ slides.js
@@ -1,53 +1,119 @@
 window.__slides = [
   {
     loc: [0, 5],
-    note: 'https://slides.therebelrobot.com'
+    note: 'c4sf.me/oss-best-practices'
   }, {
     loc: [6, 7]
   }, {
-    loc: [8, 9],
-    note: 'What Open Source Is Not'
+    loc: [7, 8]
   }, {
-    loc: [10, 11]
+    loc: [9, 10],
+    note: 'Trust: Access Source Code'
   }, {
-    loc: [12, 19]
+    loc: [10, 11],
+    note: 'Trust: Won\'t be sued'
   }, {
-    loc: [20, 21],
-    note: 'Origins'
+    loc: [11, 12],
+    note: 'Trust: Contribute back'
   }, {
-    loc: [22, 27],
-    note: 'Origins'
+    loc: [12, 13],
+    note: 'Trust: Governance and Management'
   }, {
-    loc: [28, 36],
-    note: 'Not just programming.'
+    loc: [13, 14],
+    note: 'Trust: Upgrading and Maintenance'
+  }, {
+    loc: [14, 15],
+    note: 'Trust: Safe space to work'
+  }, {
+    loc: [16, 17]
+  }, {
+    loc: [17, 18],
+    note: 'Trust: Access Source Code'
+  }, {
+    loc: [18, 19]
+  }, {
+    loc: [19, 21],
+    note: 'Trust: Won\'t be sued'
+  }, {
+    loc: [19, 21],
+    note: 'tldrlegal.com'
+  }, {
+    loc: [21, 22],
+    note: 'Permissive, Open, Basic'
+  }, {
+    loc: [22, 23],
+    note: 'Open, provides patent permissions'
+  }, {
+    loc: [23, 24],
+    note: 'Open, comprehensive patent coverage'
+  }, {
+    loc: [24, 25],
+    note: '"Don\'t bug me about this" license'
+  }, {
+    loc: [19, 25],
+    note: 'tldrlegal.com'
+  }, {
+    loc: [25, 27],
+    note: 'Trust: Contribute back'
+  }, {
+    loc: [27, 28]
+  }, {
+    loc: [28, 29]
+  }, {
+    loc: [29, 30]
+  }, {
+    loc: [30, 31]
+  }, {
+    loc: [31, 32],
+    note: 'Slack, Gitter, Email, IRC, in person, etc.'
+  }, {
+    loc: [32, 33],
+    note: 'Git Flow - c4sf.me/git-workflow'
+  }, {
+    loc: [33, 34]
+  }, {
+    loc: [34, 35]
+  }, {
+    loc: [35, 36],
+    note: 'Trust: Governance and Management'
+  }, {
+    loc: [36, 37]
   }, {
     loc: [37, 38]
   }, {
-    loc: [38, 40],
-    note: 'Have a public Github account? Congrats!'
+    loc: [38, 39]
   }, {
-    loc: [40, 42]
+    loc: [39, 40]
   }, {
-    loc: [42, 44]
+    loc: [40, 41],
+    note: 'Trust: Upgrading and Maintenance'
   }, {
-    loc: [46, 51],
-    note: 'Current Open Source Projects'
+    loc: [41, 42],
+    note: 'keepachangelog.org'
   }, {
-    loc: [52, 53]
+    loc: [42, 43],
+    note: 'semver.org'
   }, {
-    loc: [52, 57]
+    loc: [43, 44],
+    note: 'Trust: Upgrading and Maintenance'
   }, {
-    loc: [58, 59]
+    loc: [44, 46]
   }, {
-    loc: [59, 64]
+    loc: [46, 47],
+    note: 'codeclimate.com'
   }, {
-    loc: [65, 69]
+    loc: [47, 49],
+    note: 'Trust: Safe space to work'
   }, {
-    loc: [70, 74],
-    note: 'The heart of open source'
+    loc: [48, 50],
+    note: 'contributor-covenant.org'
   }, {
-    loc: [75,83]
+    loc: [50, 53]
   }, {
-    loc: [85,86]
+    loc: [7, 15],
+    note: 'Trust is key.'
+  }, {
+    loc: [0, 5],
+    note: 'c4sf.me/oss-best-practices'
   }
 ]
